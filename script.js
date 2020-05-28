3eaca8a8d8349525c2fe92f3cdb300e65ffc2cd5
tomaszPietkun <tomasz.pietkun.99@gmail.com>
2020-05-28 16:23:05 +0200

    Final push

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  const apiRoot = 'https://intense-hollows-53653.herokuapp.com/v1/task/';
+  const apiRoot = 'https://intense-hollows-53653.herokuapp.com/v1/';
   const trelloApiRoot = 'http://localhost:8080/v1/trello/';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
